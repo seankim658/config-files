c0aa66dd18c99bcc9d66e7b05c23de59d461210b
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-02-22 21:08:18 -0500

    enable git icons

---
+++ nvim/lua/custom/configs/nvim-tree.lua
@@ -0,0 +1,13 @@
+require'nvim-tree'.setup {
+  git = {
+    enable = true,
+  },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
