c55c4fbc5516dda66dbecb8b919b64d115cffff5
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-02-22 20:08:18 -0500

    add diagnotic float shortcut

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -1,5 +1,7 @@
 vim.opt.relativenumber = true
 vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", {silent = true, noremap = true})
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = false
