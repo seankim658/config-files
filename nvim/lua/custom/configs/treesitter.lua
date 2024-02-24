f94358c5e6f89be6e166ec462619b97728338a24
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-02-23 20:22:17 -0500

    fix theme issues

--- nvim/lua/custom/configs/treesitter.lua
+++ nvim/lua/custom/configs/treesitter.lua
@@ -1,11 +1,10 @@
-local treesitter = require("nvim-treesitter.configs")
-local og_treesitter = require("plugins.configs.treesitter")
+local options = require("plugins.configs.treesitter")
 
-og_treesitter.indent = {
+options.indent = {
   enable = true,
   disable = {
     "python"
   }
 }
 
-treesitter.setup(og_treesitter)
+return options
